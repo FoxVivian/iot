c0d684708059405ea605c8ad62cf9793ad4bd380
FoxVivian <vivianfox3671@gmail.com>
2024-11-05 16:27:01 +0700

    db

--- Repact_BE/src/config/database.js
+++ Repact_BE/src/config/database.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+
+
 
 // MongoDB connection URL
-const DB_URL = "mongodb+srv://neiryaeris:Fox01659943581@lab.7o83ceo.mongodb.net/?retryWrites=true&w=majority&appName=Lab";
+// const DB_URL = "mongodb+srv://neiryaeris:Fox01659943581@lab.7o83ceo.mongodb.net/?retryWrites=true&w=majority&appName=Lab";
+const DB_URL = process.env.DB_URL;
 
 // Connect to MongoDB
 mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
